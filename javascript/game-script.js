1345891ab9d2d3c73b9150b5fff89892dcdf76dd
Miguel Curbelo <mcurbeloj@gmail.com>
2022-10-14 17:51:31 +0200

    adds basic game structure, enemy component, player component, enemy movement logic, player movement logic, limits the gameboard for movement, limits player speed, applies attack mode on enemies

--- javascript/game-script.js
+++ javascript/game-script.js
@@ -1,39 +1,249 @@
-const myGameArea = {
-    canvas: document.createElement('canvas'),
-    frames: 0, // check when it's time to add obstacles
-    start: function () { // setting up canvas on the page
-      this.canvas.width = 900;
-      this.canvas.height = 700;
-      this.canvas.style = 'position: absolute; top: 50px; left: 100px; right: 100px; margin: auto; border: 1px solid black;';
-      this.context = this.canvas.getContext('2d');
-      // this.drawBackground();
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      // call updateGameArea() every 20 milliseconds
-      this.interval = setInterval(updateGameArea, 20);
-    },
-    clear: function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      },
-    stop: function () {
-        clearInterval(this.interval);
-      },
-    score: function () {
-        const points = Math.floor(this.frames / 5);
-        this.context.font = '18px serif';
-        this.context.fillStyle = 'black';
-        this.context.fillText(`Score: ${points}`, 350, 50);
-      },
-    //   drawBackground() {
-    //     this.backgroundImg = new Image();
-    //     this.backgroundImg.src ="./images/background.png"
-    //     this.ctx.drawImage(
-    //         this.backgroundImg,
-    //         this.x,
-    //         this.y,
-    //         this.width,
-    //         this.height
-    //     );
-    // }
-  };
+function startGame(){
+  myGameArea.start();
+}
 
-myGameArea.start();
+function updateGameArea() {
+  myGameArea.clear();
+  player.update();
+  player.newPos();
+  player.update();
+  //enemyMovement();
+  updateEnemy();
+  checkGameOver();
+  myGameArea.score();
+}
+
+// function enemyMovement(){
+//   for (i=0; i<myEnemy.length; i++){
+//       if ((player.attractForceBelow(myEnemy[i]) && player.attractFieldRight(myEnemy[i])) = 'true'){
+//         myEnemy[i].speedY = -1
+//         myEnemy[i].speedX = -1
+//       }
+//       if ((player.attractForceAbove(myEnemy[i]) && player.attractFieldRight(myEnemy[i])) = 'true'){
+//         myEnemy[i].speedY = +1
+//         myEnemy[i].speedX = -1
+//       }
+//       if ((player.attractForceBelow(myEnemy[i]) && player.attractFieldLeft(myEnemy[i])) = 'true'){
+//         myEnemy[i].speedY = -1
+//         myEnemy[i].speedX = +1
+//       }
+//       if ((player.attractForceAbove(myEnemy[i]) && player.attractFieldLeft(myEnemy[i])) = 'true'){
+//         myEnemy[i].speedY = +1
+//         myEnemy[i].speedX = +1
+//       }
+//   }
+// }
+
+function updateEnemy() {
+    for (i = 0; i < myEnemy.length; i++) {
+      if(myEnemy[i].x<=player.x+player.width+90 && myEnemy[i].x>player.x && myEnemy[i].y<=player.y+player.height+90 && myEnemy[i].y>player.y){
+        myEnemy[i].x += -4;
+        myEnemy[i].y += -2;
+      } 
+      else if (myEnemy[i].x<=player.x+player.width+90 && myEnemy[i].x>player.x && myEnemy[i].y+myEnemy[i].height>=player.y-90 && myEnemy[i].y+myEnemy[i].height<player.y){
+        myEnemy[i].x += -4;
+        myEnemy[i].y += +2;
+      }
+      else {
+        myEnemy[i].x += -2
+      }
+      //  if ((player.attractForceBelow(myEnemy[i]) && player.attractFieldRight(myEnemy[i]))){
+      //   myEnemy[i].x += -3
+      //   myEnemy[i].y += -1}
+      //   else if ((player.attractForceAbove(myEnemy[i]) && player.attractFieldRight(myEnemy[i]))){
+      //   myEnemy[i].speedY = -1
+      //   myEnemy[i].speedX = -3
+      //   }
+      //   else {
+      myEnemy[i].x += -2
+        // }
+      myEnemy[i].update();
+    }
+
+  myGameArea.frames += 1;
+  if (myGameArea.frames % 90 === 0) {
+    let x = myGameArea.canvas.width;
+    let y = Math.floor(Math.random() * (myGameArea.canvas.height-20));
+    myEnemy.push(new Component(20, 20, 'green', x, y));
+  }
+}
+
+function checkGameOver() {
+  const crashed = myEnemy.some(function (obstacle) {
+    return player.crashWith(obstacle);
+  });
+ 
+  if (crashed) {
+    myGameArea.stop();
+  }
+}
+
+const myEnemy = [];
+
+const myGameArea = {
+  canvas: document.createElement('canvas'),
+  frames: 0,
+  start: function () {
+    this.canvas.width = 900;
+    this.canvas.height = 500;
+    this.canvas.style = 'border: 1px solid black;';
+    this.context = this.canvas.getContext('2d');
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+
+  stop: function () {
+    clearInterval(this.interval);
+  },
+
+  score: function () {
+    const points = Math.floor(this.frames / 5);
+    this.context.font = '18px Copperplate, Papyrus, fantasy';
+    this.context.fillStyle = 'black';
+    this.context.fillText(`Score: ${points}`, 25, 25);
+  },
+};
+
+class Component {
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+  }
+ 
+  update() {
+    const ctx = myGameArea.context;
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  newPos() {
+    this.newPosX();
+    this.newPosY();
+  }
+
+  newPosX(){
+    if(this.x + this.width + this.speedX > 900){
+      this.x += (900-(this.x + this.width));
+    }
+    else if (this.x + this.speedX < 0){
+      this.x -= this.x;
+    }
+    else {
+      this.x += this.speedX;
+    }
+  }
+
+  newPosY(){
+    if(this.y + this.height + this.speedY > 500){
+      this.y += (500-(this.y + this.height));
+    }
+    else if (this.y + this.speedY < 0){
+      this.y -= this.y;
+    }
+    else {
+      this.y += this.speedY;
+    }
+}
+
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+ 
+  crashWith(obstacle) {
+    return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
+  }
+
+  attractFieldLeft() {
+    return this.x - 5;
+  }
+  attractFieldRight() {
+    return this.x + this.width + 5;
+  }
+  attractFieldTop() {
+    return this.y - 5;
+  }
+  attractFieldBottom() {
+    return this.y + this.height + 5;
+  }
+
+
+
+  // attractForceBelow(obstacle) {
+  //   return !(this.attractFieldBottom() < obstacle.top());
+  // }
+
+  // attractForceAbove(obstacle) {
+  //   return !(this.attractFieldTop() > obstacle.bottom());
+  // }
+
+  // attractForceRight(obstacle) {
+  //   return !(this.attractFieldRight() < obstacle.left());
+  // }
+
+  // attractForceLeft(obstacle) {
+  //   return !(this.attractFieldLeft() > obstacle.right());
+  // }
+}
+const player = new Component(60, 60, 'red', 420,220);
+
+document.addEventListener('keydown', (e) => {
+  switch (e.keyCode) {
+    case 38: // up arrow
+    if (player.speedY >= -7){
+      player.speedY -= 3;
+    }
+    else {
+      player.speedY = -10;
+    }
+      break;
+    case 40: // down arrow
+    if (player.speedY <= 7){
+      player.speedY += 3;
+    }
+    else {
+      player.speedY = 10;
+    }
+      break;
+    case 37: // left arrow
+    if (player.speedX >= -7){
+      player.speedX -= 3;
+    }
+    else {
+      player.speedX = -10;
+    }
+      break;
+    case 39: // right arrow
+    if (player.speedX <= 7){
+      player.speedX += 3;
+    }
+    else {
+      player.speedX = 10;
+    }
+      break;
+  }
+});
+
+document.addEventListener('keyup', (e) => {
+  player.speedX = 0;
+  player.speedY = 0;
+});
+
+
+startGame();
