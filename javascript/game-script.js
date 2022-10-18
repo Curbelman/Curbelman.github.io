08484683829fa7e294caa83b839320ccaa267259
Miguel Curbelo <mcurbeloj@gmail.com>
2022-10-18 21:40:43 +0200

    adds some css

--- javascript/game-script.js
+++ javascript/game-script.js
@@ -1,1 +1,11 @@
-startGame();
+window.onload = () => {
+   
+  //const canvas = document.querySelector("#myCanvas")
+  const intro = document.querySelector('.intro')
+
+  document.getElementById('start').onclick = () => {
+      intro.style.display="none";
+      //caja.classList.add('hidden')
+      startGame();
+  }
+}
